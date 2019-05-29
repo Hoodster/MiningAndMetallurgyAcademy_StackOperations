062aa26a7ff198626b2d939837f5e1a2ac4d43eb
Jakub Porębski <jporebski@future-processing.com>
2019-05-29 20:13:35 +0200

    Changed stack operations and added new tests

--- MiningAndMetallurgyAcademy_/MiningAndMetallurgyAcademy_.cpp
+++ MiningAndMetallurgyAcademy_/MiningAndMetallurgyAcademy_.cpp
@@ -1,45 +1,83 @@
-
 #include <iostream>
 
 struct stack 
 {
 	double value;
-	stack* next = NULL;
+	stack* previous;
 };
 
-void push(stack *stk, double y) 
-{
-	stack* temp;
-	temp = stk;
-	stk = new stack;
-	stk->value = y;
-	stk->next = temp;
-}
 
-double pop(stack *stk)
+void push(stack& head, double y)
 {
-	if (stk != NULL)
+	stack* temp = new stack;
+	(*temp).value = head.value;
+	(*temp).previous = head.previous;
+
+	head.value = y;
+
+	if (head.value != NULL) 
 	{
-		double stkVar = 0.0;
-		stack* temp;
-		stkVar = stk->value;
-		temp = stk->next;
-		delete stk;
-		stk = temp;
-		return stkVar;
+		head.previous = temp;
 	}
 }
 
-double top(stack *stk)
+double top(stack& head)
 {
-	return stk->value;
+	return head.value;
+}
+
+double pop(stack& head)
+{
+	if (head.previous != NULL)
+	{
+		double headVar = 0.0;
+		stack* temp = head.previous;
+		stack* previousValue = head.previous;
+		headVar = (*previousValue).value;
+		temp = head.previous;
+		head = *temp;
+		return headVar;
+	}
 }
 
 int main()
 {
-	stack sta1;
-	push(&sta1, 1.0);
-	push(&sta1, -7.0);
-	pop(&sta1);
-	std::cout << top(&sta1) << std::endl;
+	try {
+		stack* head = new stack;
+		head->value = NULL;
+		push(*head, 1.5);
+		push(*head, -7.0);
+		push(*head, 9.0);
+		push(*head, 10.45);
+
+		std::cout << "Printing all numbers in stack." << std::endl;
+		std::cout << top(*head) << std::endl;
+		std::cout << top(*head->previous) << std::endl;
+		std::cout << top(*head->previous->previous) << std::endl;
+		std::cout << top(*head->previous->previous->previous) << std::endl << std::endl;
+
+		std::cout << "Deleting numbers" << std::endl;
+		std::cout << top(*head) << std::endl;
+		pop(*head);
+		std::cout << top(*head) << std::endl;
+		pop(*head);
+		std::cout << top(*head) << std::endl;
+		pop(*head);
+		std::cout << top(*head) << std::endl;
+		pop(*head);
+		std::cout << top(*head) << std::endl << std::endl << std::endl;
+
+		std::cout << "Adding numbers (showing top stack value)" << std::endl;
+		std::cout << "Current stack first element: " << top(*head) << " (empty) "  << std::endl;
+		push(*head, 2.0);
+		std::cout << top(*head) << std::endl;
+		push(*head, 3.3);
+		std::cout << top(*head) << std::endl;
+		push(*head, 44.43243);
+		std::cout << top(*head) << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what();
+	}
 }
